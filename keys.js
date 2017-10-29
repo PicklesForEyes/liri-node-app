425640768291aa75c42b0592843449a68e244fe5
Benjamin Lawson <lawbe101@yahoo.com>
2017-10-29 18:12:33 -0400

    initial files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is running');
+
+var twitterKeys = {
+  consumer_key: 'ppZIeBb7OAVEZ3eho4xH3vvIG',
+  consumer_secret: 'IZM5m8rEy5XGl5EA6kb7zTXp9ve2mM9DIQULBSb6t7nCu6Cxc0',
+  access_token_key: '924758224540241921-D7LDbLPwwVaLrE8vrP7vYDa6joltgOy',
+  access_token_secret: '  PpMH0jN4No88Xa6pD5b9Y1iSIKRBwefgMUc37bY15MoFh',
+}
