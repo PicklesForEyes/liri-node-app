cc76dcec5688c069345f6cbc136263305c520ca1
Benjamin Lawson <lawbe101@yahoo.com>
2017-10-29 18:57:27 -0400

    added spotify keys and exported them

--- keys.js
+++ keys.js
@@ -1,5 +1,10 @@
 // console.log('this is running');
 
+var spotifyKeys = {
+  id: '1dcf08754d23473296c094b285e8a953',
+  secret: '8533eafc026d44b48797f187ece9d77b'
+}
+
 var twitterKeys = {
   consumer_key: 'ppZIeBb7OAVEZ3eho4xH3vvIG',
   consumer_secret: 'IZM5m8rEy5XGl5EA6kb7zTXp9ve2mM9DIQULBSb6t7nCu6Cxc0',
@@ -7,4 +12,7 @@ var twitterKeys = {
   access_token_secret: '  PpMH0jN4No88Xa6pD5b9Y1iSIKRBwefgMUc37bY15MoFh',
 }
 
-module.exports = twitterKeys;
+module.exports = {
+  twitter: twitterKeys,
+  spotify: spotifyKeys
+};
